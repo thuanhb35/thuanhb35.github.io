b9578f63fb1fd447dd9a64e77f79bafdce10bd5d
thuanhb35 <146250297+thuanhb35@users.noreply.github.com>
2023-12-13 12:17:51 -0600

    end ofclass
    add dino picture

--- Capstone-DinoGame/sketch.js
+++ Capstone-DinoGame/sketch.js
@@ -4,12 +4,19 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+let dinoImage
 
+function preload(){
+  dinoImage = loadImage("assets/dino.png")
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(220);  
+  image(dinoImage, 100, 100, 50, 50);
 }
+
+
